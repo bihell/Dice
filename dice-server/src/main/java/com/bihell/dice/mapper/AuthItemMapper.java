a0667955684fc59d9083be83ab39cab64cde2031
tpxcer <tpxcer@outlook.com>
2019-12-19 16:35:10 +0800

    feat:增加菜单item 类和 mapper

---
+++ dice-server/src/main/java/com/bihell/dice/mapper/AuthItemMapper.java
@@ -0,0 +1,13 @@
+package com.bihell.dice.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bihell.dice.model.domain.AuthItem;
+
+/**
+ * User Mapper
+ *
+ * @author bihell
+ * @since 2017/7/12 21:34
+ */
+public interface AuthItemMapper extends BaseMapper<AuthItem> {
+}

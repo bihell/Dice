6665a2f5cdce81fb3e5d65f147ae4bb57c8a918e
tpxcer <tpxcer@outlook.com>
2020-01-10 13:46:17 +0800

    doc: 权限数据初始化

--- dice-server/src/main/java/com/bihell/dice/controller/admin/AuthController.java
+++ dice-server/src/main/java/com/bihell/dice/controller/admin/AuthController.java
@@ -1,6 +1,5 @@
 package com.bihell.dice.controller.admin;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.bihell.dice.controller.BaseController;

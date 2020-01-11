063e080f20ba032a19c4d50474f14f424bba0d00
tpxcer <tpxcer@outlook.com>
2020-01-11 10:46:08 +0800

    perf:将管理前端镜像去掉，编译好以后直接放到openresty 容器里面部署

---
+++ deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#====================================================
+#	System Request:Ubuntu 18.04+
+#	Author:	chenchao
+#	Dscription: Dice 部署脚本
+#	Version: 1.0
+#	email:tpxcer@outlook.com
+#	Official document: https://bihell.gitee.io/big-data/project/dice.html
+#====================================================
+
+#fonts color
+Green="\033[32m"
+Red="\033[31m"
+Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+Font="\033[0m"
+
+#notification information
+Info="${Green}[信息]${Font}"
+OK="${Green}[OK]${Font}"
+Error="${Red}[错误]${Font}"
+
+judge(){
+    if [[ $? -eq 0 ]];then
+        echo -e "${OK} ${GreenBG} $1 完成 ${Font}"
+        sleep 1
+    else
+        echo -e "${Error} ${RedBG} $1 失败${Font}"
+        exit 1
+    fi
+}
+
+# 安装 Dice-Admin 依赖
+cd ./dice-admin/ && npm install
+judge "安装 Dice-Admin 项目依赖"
+
+# 编译 Dice-Admin 项目
+npm run build:prod
+judge "编译 Dice-Admin 项目"
+cd -
+
+# docker 部署
+docker-compose down
+judge "停止当前容器"
+docker-compose up --build -d
+judge "生成新容器并在后台启动"

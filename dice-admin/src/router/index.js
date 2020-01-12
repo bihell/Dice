a34c80c59250cbaafb4561a05d15376c18d2d214
tpxcer <tpxcer@outlook.com>
2020-01-12 22:37:33 +0800

    pref: 调整路由模式

--- dice-admin/src/router/index.js
+++ dice-admin/src/router/index.js
@@ -112,7 +112,8 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
+  base: 'admin',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })

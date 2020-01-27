82604c0d246497887901cc628213d09b295e97c0
tpxcer <tpxcer@outlook.com>
2020-01-27 16:41:53 +0800

    feat:新增「任务管理」

--- dice-server/src/main/java/com/bihell/dice/service/task/TaskService.java
+++ dice-server/src/main/java/com/bihell/dice/service/task/TaskService.java
@@ -1,9 +1,21 @@
 package com.bihell.dice.service.task;
 
+import com.bihell.dice.model.tool.Task;
+
 /**
  * @author haseochen
  */
 public interface TaskService {
 
+    /**
+     * @param task
+     * @return
+     */
+    Task save(Task task);
 
+    public Task disable(Long id);
+
+    Task enable(Long id);
+
+    void delete(Long id);
 }

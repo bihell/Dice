4388b25f560106851f8c628842b4e5645bfeed6c
chenchao <chenchao@hujiang.com>
2020-09-04 11:38:30 +0800

    chore:增加mybatis三个测试项

---
+++ dice-server/src/test/java/com/bihell/dice/service/UserMapperTests.java
@@ -0,0 +1,41 @@
+package com.bihell.dice.service;
+
+import com.bihell.dice.mapper.auth.UserMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserMapperTests {
+
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    @Rollback
+    public void test() throws Exception {
+        userMapper.insert("test", "test");
+//        User u = userMapper.findByName("AAA");
+//        Assert.assertEquals(20, u.getAge().intValue());
+    }
+
+    @Transactional
+    @Test
+    @Rollback
+    public void insertByMap() throws Exception {
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", "test");
+        map.put("passwdMd5", "test");
+        userMapper.insertByMap(map);
+    }
+
+}

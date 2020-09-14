ceda71e26c27dcc0f3cd9a3a34f3f2151657b87d
tpxcer <tpxcer@outlook.com>
2020-09-14 16:37:09 +0800

    build:初始模块化

---
+++ dice-server/bootstrap/src/main/java/com/bihell/dice/model/dto/Archive.java
@@ -0,0 +1,27 @@
+package com.bihell.dice.model.dto;
+
+import com.bihell.dice.model.blog.Article;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 归档 Dto
+ *
+ * @author bihell
+ * @since 2017/9/21 11:24
+ */
+@Data
+@Accessors(chain = true)
+public class Archive {
+
+    private String dateStr;
+
+    private LocalDateTime date;
+
+    private Integer count;
+
+    private List<Article> articles;
+}
---
+++ dice-server/bootstrap/src/main/java/com/bihell/dice/model/tool/Task.java
@@ -0,0 +1,43 @@
+package com.bihell.dice.model.tool;
+
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.bihell.dice.model.BaseEntity;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.time.LocalDateTime;
+
+
+/**
+ * @author haseochen
+ */
+
+@Setter
+@Getter
+@Accessors(chain = true)
+public class Task extends BaseEntity<Task> {
+
+    @TableId
+    private Long id;
+
+    private String name;
+
+    private String jobGroup;
+
+    private String jobClass;
+
+    private String note;
+
+    private String cron;
+
+    private boolean concurrent;
+
+    private String data;
+
+    private LocalDateTime execAt;
+
+    private String execResult;
+
+    private Integer status;
+}

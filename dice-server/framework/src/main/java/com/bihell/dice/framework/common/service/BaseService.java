8debfce18d43a30a6f071fa4c4d19e1e4492eae3
tpxcer <tpxcer@outlook.com>
2020-09-23 22:53:23 +0800

    chorn:增加待实现配置

---
+++ dice-server/framework/src/main/java/com/bihell/dice/framework/common/service/BaseService.java
@@ -0,0 +1,10 @@
+package com.bihell.dice.framework.common.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * 公共Service接口 todo
+ */
+public interface BaseService<T> extends IService<T> {
+
+}

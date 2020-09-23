457d79032ebfaf7a5cf22b3d19a4d5fc8f2bed10
tpxcer <tpxcer@outlook.com>
2020-09-23 18:12:46 +0800

    chore:增加待实现配置

--- dice-server/framework/src/main/java/com/bihell/dice/framework/util/UUIDUtil.java
+++ dice-server/framework/src/main/java/com/bihell/dice/framework/util/UUIDUtil.java
@@ -8,8 +8,7 @@ import java.util.UUID;
 public class UUIDUtil {
 
     public static String getUuid(){
-        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
-        return uuid;
+        return UUID.randomUUID().toString().replaceAll("-", "");
     }
     
 }

6a1f577293223b164dc5524316d591746b7a9015
tpxcer <tpxcer@outlook.com>
2020-09-27 13:28:30 +0800

    chore:系统操作日志增加deleted字段

--- dice-server/bootstrap/src/main/resources/init.sql
+++ dice-server/bootstrap/src/main/resources/init.sql
@@ -394,7 +394,8 @@ create table sys_operation_log
     device_model      varchar(100)                       null comment '移动端设备型号',
     remark            varchar(200)                       null comment '备注',
     create_time       datetime default CURRENT_TIMESTAMP null comment '创建时间',
-    update_time       datetime                           null comment '修改时间'
+    update_time       datetime                           null comment '修改时间',
+    deleted           int      default 0                 null comment '逻辑删除，0：未删除，1：已删除'
 )
     comment '系统操作日志';
 

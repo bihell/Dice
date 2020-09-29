cd925066850ffd0e4f1cfcc7be586d9f3480fd9a
tpxcer <tpxcer@outlook.com>
2020-09-29 14:09:10 +0800

    chore:增加knife4j地址访问地址显示

--- dice-server/bootstrap/src/main/java/com/bihell/dice/StartedListener.java
+++ dice-server/bootstrap/src/main/java/com/bihell/dice/StartedListener.java
@@ -39,7 +39,9 @@ public class StartedListener implements ApplicationListener<ApplicationStartedEv
         String baseUrl = optionService.getBaseUrl();
 
         if (swaggerProperties.isEnable()) {
-            log.info(AnsiOutput.toString(AnsiColor.BRIGHT_BLUE, "Dice api doc was enabled at ",baseUrl,"/swagger-ui.html"));
+            log.info(AnsiOutput.toString(AnsiColor.BRIGHT_BLUE, "Dice api Swagger doc was enabled at ",baseUrl,"/swagger-ui.html"));
+            log.info(AnsiOutput.toString(AnsiColor.BRIGHT_BLUE, "Dice api Knife4j doc was enabled at ",baseUrl,"/doc.html"));
+
         }
         log.info(AnsiOutput.toString(AnsiColor.BRIGHT_YELLOW, "Dice has started successfully!"));
     }

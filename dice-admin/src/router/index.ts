aad94fdd88732cf8728cd1f10d746012f4bbe6cd
tpxcer <tpxcer@outlook.com>
2021-06-08 18:44:31 +0800

    chore(dice-admin):framework update

--- dice-admin/src/router/index.ts
+++ dice-admin/src/router/index.ts
@@ -1,16 +1,16 @@
 import type { RouteRecordRaw } from 'vue-router';
 import type { App } from 'vue';
 
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { basicRoutes, LoginRoute } from './routes';
 import { REDIRECT_NAME } from './constant';
 
 const WHITE_NAME_LIST = [LoginRoute.name, REDIRECT_NAME];
 
 // app router
-const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_PUBLIC_PATH),
-  routes: (basicRoutes as unknown) as RouteRecordRaw[],
+export const router = createRouter({
+  history: createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH),
+  routes: basicRoutes as unknown as RouteRecordRaw[],
   strict: true,
   scrollBehavior: () => ({ left: 0, top: 0 }),
 });
@@ -29,5 +29,3 @@ export function resetRouter() {
 export function setupRouter(app: App<Element>) {
   app.use(router);
 }
-
-export default router;

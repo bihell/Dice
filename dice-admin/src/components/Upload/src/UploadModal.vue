22cf6abaf94b8a7a3bf1b4e11cbbceaa124a4539
tpxcer <tpxcer@outlook.com>
2021-06-24 18:46:31 +0800

    chore(dice-admin):framework update

--- dice-admin/src/components/Upload/src/UploadModal.vue
+++ dice-admin/src/components/Upload/src/UploadModal.vue
@@ -252,7 +252,7 @@
         for (const item of fileListRef.value) {
           const { status, responseData } = item;
           if (status === UploadResultStatus.SUCCESS && responseData) {
-            fileList.push(responseData.url);
+            fileList.push(responseData.result);
           }
         }
         // 存在一个上传成功的即可保存

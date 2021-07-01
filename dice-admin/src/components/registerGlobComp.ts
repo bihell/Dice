6ad76bc6753817c906cc1d1d26550fa87a257f8f
tpxcer <tpxcer@outlook.com>
2021-07-01 17:00:45 +0800

    chore(dice-admin):framework update

--- dice-admin/src/components/registerGlobComp.ts
+++ dice-admin/src/components/registerGlobComp.ts
@@ -4,42 +4,7 @@ import { Button } from './Button';
 import {
   // Need
   Button as AntButton,
-  // Optional
-  Select,
-  Alert,
-  Checkbox,
-  DatePicker,
-  Radio,
-  Switch,
-  Card,
-  List,
-  Tabs,
-  Descriptions,
-  Tree,
-  Table,
-  Divider,
-  Modal,
-  Drawer,
-  Dropdown,
-  Tag,
-  Tooltip,
-  Badge,
-  Popover,
-  Upload,
-  Transfer,
-  Steps,
-  PageHeader,
-  Result,
-  Empty,
-  Avatar,
-  Menu,
-  Breadcrumb,
-  Form,
   Input,
-  Row,
-  Col,
-  Spin,
-  InputNumber,
   Layout,
 } from 'ant-design-vue';
 
@@ -50,42 +15,5 @@ export function registerGlobComp(app: App) {
     app.component(comp.name || comp.displayName, comp);
   });
 
-  app
-    .use(Select)
-    .use(Alert)
-    .use(Breadcrumb)
-    .use(Checkbox)
-    .use(DatePicker)
-    .use(Radio)
-    .use(Switch)
-    .use(Card)
-    .use(List)
-    .use(Descriptions)
-    .use(Tree)
-    .use(Table)
-    .use(Divider)
-    .use(Modal)
-    .use(Drawer)
-    .use(Dropdown)
-    .use(Tag)
-    .use(Tooltip)
-    .use(Badge)
-    .use(Popover)
-    .use(Upload)
-    .use(Transfer)
-    .use(Steps)
-    .use(PageHeader)
-    .use(Result)
-    .use(Empty)
-    .use(Avatar)
-    .use(Menu)
-    .use(Tabs)
-    .use(Form)
-    .use(Input)
-    .use(Row)
-    .use(Col)
-    .use(Spin)
-    .use(InputNumber)
-    .use(Button)
-    .use(Layout);
+  app.use(Input).use(Button).use(Layout);
 }

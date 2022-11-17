5fd4fcf7cb1851d0f35af1c14ca71c6ddbeafa62
tpxcer <tpxcer@outlook.com>
2022-11-17 16:10:28 +0800

    refactor(dice-server): remove generator module

--- dice-server/generator/src/test/java/com/bihell/dice/generator/test/GeneratorTestApplication.java
+++
@@ -1,7 +0,0 @@
-package com.bihell.dice.generator.test;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication(scanBasePackages = "com.bihell.dice.generator")
-public class GeneratorTestApplication {
-}

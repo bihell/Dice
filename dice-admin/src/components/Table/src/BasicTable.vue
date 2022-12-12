b61bba2a7d99fa54e0614ec1dc0bcd3bc82bf037
tpxcer <tpxcer@outlook.com>
2022-12-12 10:43:19 +0800

    fix(dice-admin):表格查询条件下拉框文本超长显示错乱

--- dice-admin/src/components/Table/src/BasicTable.vue
+++ dice-admin/src/components/Table/src/BasicTable.vue
@@ -75,6 +75,7 @@
   import { warn } from '/@/utils/log';
 
   export default defineComponent({
+    name:'BasicTable',
     components: {
       Table,
       BasicForm,
@@ -136,6 +137,7 @@
         getRowSelection,
         getRowSelectionRef,
         getSelectRows,
+        setSelectedRows,
         clearSelectedRowKeys,
         getSelectRowKeys,
         deleteSelectRowByKey,
@@ -288,6 +290,7 @@
       const tableAction: TableActionType = {
         reload,
         getSelectRows,
+        setSelectedRows,
         clearSelectedRowKeys,
         getSelectRowKeys,
         deleteSelectRowByKey,
@@ -375,6 +378,7 @@
       padding: 16px;
 
       .ant-form {
+        width: 100%;
         padding: 12px 10px 6px;
         margin-bottom: 16px;
         background-color: @component-background;

fd00dcfa3b19870dae9acfc1508139e353b31687
tpxcer <tpxcer@outlook.com>
2023-04-10 16:49:38 +0800

    fix(dice-admin): checked with searching

--- dice-admin/src/components/Tree/src/types/tree.ts
+++ dice-admin/src/components/Tree/src/types/tree.ts
@@ -100,7 +100,7 @@ export const treeProps = buildProps({
   },
 
   checkedKeys: {
-    type: Array as PropType<CheckKeys>,
+    type: [Array, Object] as PropType<CheckKeys>,
     default: () => [],
   },
 
@@ -134,7 +134,7 @@ export const treeProps = buildProps({
     type: Boolean,
     default: false,
   },
-  treeWrapperClassName: String
+  treeWrapperClassName: String,
 });
 
 export type TreeProps = ExtractPropTypes<typeof treeProps>;

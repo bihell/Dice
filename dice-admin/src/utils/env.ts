ff0eacc276728d309ea71371bceb3f6379b1a21f
tpxcer <tpxcer@outlook.com>
2023-08-07 11:39:58 +0800

    fix(dice-admin):修正VITE_GLOB_APP_TITLE' of 'C' as it is undefined 错误

--- dice-admin/src/utils/env.ts
+++ dice-admin/src/utils/env.ts
@@ -30,7 +30,9 @@ export function getAppEnvConfig() {
   const ENV = (import.meta.env.DEV
     ? // Get the global configuration (the configuration will be extracted independently when packaging)
       (import.meta.env as unknown as GlobEnvConfig)
-    : window[ENV_NAME as any]) as unknown as GlobEnvConfig;
+    : window[ENV_NAME as any]
+    ? window[ENV_NAME as any]
+    : (import.meta.env as unknown as GlobEnvConfig)) as unknown as GlobEnvConfig;
 
   const { VITE_GLOB_APP_TITLE, VITE_GLOB_API_URL, VITE_GLOB_API_URL_PREFIX, VITE_GLOB_UPLOAD_URL } =
     ENV;
